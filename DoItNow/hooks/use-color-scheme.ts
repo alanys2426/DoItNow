966a08d19cb6f53fc4363b4d2ab8a73cb99f0332
alanys2426 <al162520@ucf.edu>
2026-04-08 19:49:42 -0400

    Add other color scheme helper hook

--- DoItNow/hooks/use-color-scheme.ts
+++ DoItNow/hooks/use-color-scheme.ts
@@ -1,1 +1,9 @@
-export { useColorScheme } from 'react-native';
+
+// Import a custom hook that gives theme info (light/dark)
+import { useThemeMode } from '@/context/theme-mode-context';
+
+// Get current color scheme
+export function useColorScheme() {
+  const { resolvedScheme } = useThemeMode();
+  return resolvedScheme;
+}
